93c792e6e623ca0c65466e57006822ec97e67adc
alzaq <ales@tapmates.com>
2018-02-27 14:04:52 +0100

    Tutorial 2-func - new func

--- 2-func/2-func.go
+++ 2-func/2-func.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func genName(char string, number int) string {
@@ -26,6 +27,23 @@ func valInfo(x, y int) (min int, max int, avg int) {
 	return
 }
 
+func formateTime(date time.Time) string {
+	return fmt.Sprintf("%d:%d:%d", date.Hour(), date.Minute(), date.Second())
+}
+
+func getNameOfDay(date time.Time) string {
+	switch date.Weekday() {
+	case 1:
+		return "Pondeli"
+	case 2:
+		return "Utery"
+	case 3:
+		return "Streda"
+	default:
+		return fmt.Sprintf("Day: %d", date.Weekday())
+	}
+}
+
 func main() {
 	fmt.Println("--- 2 FUNC ---")
 	fmt.Printf("Hello %s!", "Ales")
@@ -39,4 +57,9 @@ func main() {
 
 	min, max, avg := valInfo(a, b)
 	fmt.Println(min, max, avg)
+
+	fmt.Println(formateTime(time.Now()))
+
+	var dayOfWeek = getNameOfDay(time.Now())
+	fmt.Println(dayOfWeek)
 }
